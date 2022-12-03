f70803305edaafd2eca06477b2a7ae9d73a775a6
Emin Deniz <emindeniz99@gmail.com>
2022-12-03 08:32:51 +0300

    add /messages endpoints

---
+++ functions/messages/[hash].js
@@ -0,0 +1,13 @@
+export function onRequestGet(context) {
+    const hash=JSON.stringify(context.params.hash);
+
+    const value = await MESSAGES_KV.get(hash);
+    if (value === null) {
+        return new Response("Value not found", {
+            status: 404
+        });
+    }
+
+    return new Response(value);
+
+  }
